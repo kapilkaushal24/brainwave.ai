f320df5c5b3779468f82bd527b58002df9d2d40e
kapilkauushal24 <kapilkaushal466@gmail.com>
2024-10-12 23:05:14 +0530

    brainwave-ai

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./Components/Benefits";
+import Collabortion from "./Components/Collabortion";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+import Pricing from "./Components/Pricing";
+import Roadmap from "./Components/Roadmap";
+import Services from "./Components/Services";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header/>
+        <Hero/>
+        <Benefits/>
+        <Collabortion/>
+        <Services/>
+        <Pricing/>
+        <Roadmap/>
+        <Footer/>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
